ff9109733e757ee5cad2ad142e2424a9a343d510
Max Bernwieser <max.bernwieser@web.de>
2022-03-19 14:42:53 +0100

    Scope plugin-template

--- public/plugins/template.js
+++ public/plugins/template.js
@@ -1,28 +1,31 @@
-// ---------------- CHANGE THIS TO THE NAME OF YOUR PLUGIN -------------------
-const pluginName = 'template';
+(function () {
+  // ---------------- CHANGE THIS TO THE NAME OF YOUR PLUGIN -------------------
+  const pluginName = 'template';
 
-// eslint-disable-next-line no-unused-vars
-window.addEventListener('THROW', (event) => {
-  // if the user disabled the plugin via the web-ui exit directly
-  if (!window.AUTODARTS_CALLER_PLUGINS[pluginName]) {
-    return;
-  }
+  // eslint-disable-next-line no-unused-vars
+  window.addEventListener('THROW', (event) => {
+    // if the user disabled the plugin via the web-ui exit directly
+    if (!window.AUTODARTS_CALLER_PLUGINS[pluginName]) {
+      return;
+    }
 
-  // --------------- ADD YOUR CUSTOM LOGIC HERE -------------------
-  //console.log(event);
-  //event.notificationService.success('Throw registered!');
-  //event.soundService.playAudio('your-sound-file.mp3');
-});
+    // --------------- ADD YOUR CUSTOM LOGIC HERE -------------------
+    //console.log(event);
+    //event.notificationService.success('Throw registered!');
+    //event.soundService.playAudio('your-sound-file.mp3');
+  });
 
-// -------------- DO NOT CHANGE ANYTHING BELOW HERE ---------------------
+  // -------------- DO NOT CHANGE ANYTHING BELOW HERE ---------------------
 
-window.addEventListener('INIT_PLUGINS', () => {
-  let enabled = true;
+  window.addEventListener('INIT_PLUGINS', () => {
+    let enabled = true;
 
-  if (`AUTODARTS_CALLER_PLUGINS_${pluginName}` in localStorage) {
-    enabled =
-      localStorage.getItem(`AUTODARTS_CALLER_PLUGINS_${pluginName}`) === 'true';
-  }
+    if (`AUTODARTS_CALLER_PLUGINS_${pluginName}` in localStorage) {
+      enabled =
+        localStorage.getItem(`AUTODARTS_CALLER_PLUGINS_${pluginName}`) ===
+        'true';
+    }
 
-  window.AUTODARTS_CALLER_PLUGINS[pluginName] = enabled;
-});
+    window.AUTODARTS_CALLER_PLUGINS[pluginName] = enabled;
+  });
+})();
