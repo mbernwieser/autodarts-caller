68031e8e3fe15fa6c97335dfe0c857006d7e18f1
Max Bernwieser <max.bernwieser@web.de>
2022-03-21 17:01:23 +0100

    Add restart policy "unless-stopped" to docker container

--- public/install_autodarts_caller.sh
+++ public/install_autodarts_caller.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker create --name autodarts-caller -v ~/autodarts-caller:/usr/share/nginx/html:ro -v ~/autodarts-caller/nginx.conf:/etc/nginx/conf.d/default.conf:ro -p 8080:80 nginx:alpine
+sudo docker create --name autodarts-caller --restart unless-stopped -v ~/autodarts-caller:/usr/share/nginx/html:ro -v ~/autodarts-caller/nginx.conf:/etc/nginx/conf.d/default.conf:ro -p 8080:80 nginx:alpine
